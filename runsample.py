478c530e41fe75717812a29a9c2ae7c99901813b
Tyghe Vallard <vallardt@gmail.com>
2014-04-24 14:18:00 -0400

    Seems to be running

--- runsample.py
+++ runsample.py
@@ -6,6 +6,7 @@ import sys
 import os.path
 from glob import glob
 import subprocess
+import re
 
 import fix_fastq
 
@@ -19,6 +20,63 @@ def run_sample( fqdir, outdir ):
     flash_o = os.path.join( outdir, 'flash' )
     prefix = 'out'
     flasho = flash( *fixfqs, o=prefix, d=flash_o )
+    truseq = os.path.join( os.path.dirname(__file__), 'truseq.txt' )
+    btrim_o = os.path.join( outdir, 'btrim' )
+    bfiles = btrim_files( [f for f in flasho if f.endswith('.fastq')], btrim_o, p=truseq, b=300, P=True, Q=True, S=True, l=100 )
+    projdir = os.path.join( outdir, 'newbler_assembly' )
+    run_assembly( bfiles, o=projdir )
+
+def run_assembly( fastqs, **options ):
+    projdir = new_assembly( options.get('o',None) )
+    replace_newbler_settings( projdir, fastqs )
+    # In case o was not in options we set it again
+    cmd = ['runProject', projdir]
+    out = subprocess.check_output( cmd, stderr=subprocess.STDOUT )
+    if 'Usage:' in out:
+        print ' '.join(cmd)
+        print out
+        return 1
+    else:   
+        return 0
+
+def new_assembly( projdir=None ):
+    cmd = ['newAssembly']
+    # If project was specified then
+    if projdir is not None:
+        cmd += [projdir]
+    
+    out = subprocess.check_output( cmd )
+    p = 'Created assembly project directory (.*)'
+    return re.match( p, out ).group(1)
+
+def newbler_fastq_read_files( fastqs ):
+    filet = '''
+    <File>
+      <Path>{}</Path>
+      <Type>Fastq</Type>
+      <FastqScoreType>Standard</FastqScoreType>
+      <FastqAccnoType>General</FastqAccnoType>
+      <IsPairedEnd>false</IsPairedEnd>
+      <Rescore>false</Rescore>
+      <removeFlag>false</removeFlag>
+    </File>'''
+
+    xml = '<ReadFiles>\n'
+    xml += '\t<SetReadCount>{}</SetReadCount>'.format(len(fastqs))
+    for f in fastqs:
+        xml += filet.format(f)
+    xml += '\n\t</ReadFiles>'
+    return xml
+
+def replace_newbler_settings( projpath, fastqs ):
+    xmlfile = os.path.join( projpath, 'assembly/454AssemblyProject.xml' )
+    fh = open( xmlfile )
+    xml = fh.read()
+    fh.close()
+    readxml = newbler_fastq_read_files( fastqs )
+    newxml = re.sub( '<ReadFiles>\n.*\n.*</ReadFiles>', readxml, xml, 1, re.DOTALL )
+    with open( xmlfile, 'w' ) as fh:
+        fh.write( newxml )
 
 def build_options( **options ):
     '''
@@ -32,6 +90,32 @@ def build_options( **options ):
         else:
             cmd += ['-'+o,str(v)]
     return cmd
+
+def btrim_files( fqlist, outdir, **btrimops ):
+    ofiles = []
+    if not os.path.exists(outdir):
+        os.makedirs( outdir )
+
+    for f in fqlist:
+        # Have to set the input file in the options
+        of = os.path.join( outdir, os.path.basename(f) ).replace( '.fastq', '.btrim.fastq' )
+        btrimops['t'] = f
+        btrimops['o'] = of
+        btrim( **btrimops )
+        ofiles.append( of )
+    return ofiles
+
+def btrim( **options ):
+    cmd = ['btrim'] + build_options( **options )
+    print ' '.join(cmd)
+    try:
+        out = subprocess.check_output( cmd, stderr=subprocess.STDOUT )
+    except subprocess.CalledProcessError as e:
+        print "Command run: {}".format(' '.join(cmd) )
+        print "btrim returned with return code ".format( str(e.returncode) )
+        print "and error: " + e.output
+        return e.returncode
+    return 0
 
 def flash( mate1, mate2, **options ):
     '''
@@ -48,10 +132,12 @@ def flash( mate1, mate2, **options ):
     '''
     cmd = ['flash'] + build_options(**options) + [mate1,mate2]
     try:
-        out = subprocess.check_output( cmd )
+        out = subprocess.check_output( cmd, stderr=subprocess.STDOUT )
     except subprocess.CalledProcessError as e:
-        print "flash returned with return code " + e.returncode
+        print "Command run: {}".format(' '.join(cmd) )
+        print "flash returned with return code {}".format( str(e.returncode) )
         print "and error: " + e.output
+        return []
     prefix = options.get('o','out')
     outdir = options.get('d','')
     files = (
--- test/test_runsample.py
+++ test/test_runsample.py
@@ -5,6 +5,8 @@ class Base( common.Base ):
     fixdir = join( dirname(__file__), 'fixtures', 'fix_fastq' )
     fqs = glob( join( fixdir, '*.fastq' ) )
     bnfqs = [basename(fq) for fq in fqs]
+    truseq = join(dirname(dirname(__file__)),'truseq.txt')
+    assemprojxml = join(fixdir,'454AssemblyProject.xml')
     flash_files = (
         '{}.extendedFrags.fastq',
         '{}.notCombined_1.fastq',
@@ -18,6 +20,16 @@ class Base( common.Base ):
         for f in self.flash_files:
             files.append( join( outdir, f ).format(prefix) )
         return files
+
+    def flash( self, prefix='out', outdir='flash' ):
+        from runsample import flash
+        # Files from flash
+        return [f for f in flash( *self.fqs, d=outdir, o=prefix ) if f.endswith('.fastq')]
+
+    def fqs_in_xml( self, fqlist, xml ):
+        for fqfile in fqlist:
+            ok_( fqfile in xml, '{} did not make it into 454AssemblyProject.xml'.format(fqfile) )
+        ok_( '<SetReadCount>{}</SetReadCount>'.format(len(self.fqs)), 'Did not set read count correctly' )
 
 class TestGetMiSeqReads(Base):
     def _C( self, *args, **kwargs ):
@@ -54,6 +66,128 @@ class TestUnitFlash(Base):
         for f in flashout:
             ok_( exists( f ), 'Did not create {}'.format(f) )
 
+    def test_runs_default( self ):
+        efiles = self.flash_output_files( 'out', '' )
+        flashout = self._C( *self.fqs )
+        eq_( set([]), set(efiles) - set(flashout) )
+        for f in flashout:
+            ok_( exists( f ), 'Did not create {}'.format(f) )
+
+    def test_fail_run( self ):
+        eq_( [], self._C( '1', '2' ), 'Did not return empty list on bad run' )
+
+class TestUnitBtrim(Base):
+    def _C( self, *args, **kwargs ):
+        from runsample import btrim
+        return btrim( *args, **kwargs )
+
+    def test_runs( self ):
+        from runsample import flash
+        flashout = flash( *self.fqs )
+        inf = flashout[0]
+        outf = inf.replace('.fastq','.btrim.fastq')
+        r = self._C( p=self.truseq, t=inf, o=outf, b=300, c=True, Q=True, S=True, l=100 )
+        ok_( exists(outf), 'btrim did not create {}'.format(outf) )
+        ok_( os.stat(inf).st_size != os.stat(outf).st_size, 'Did not trim input file. Input and output file are same size still' )
+
+    def test_runs_outdir_noexist( self ):
+        # btrim does not create full path to file
+        inf = self.flash()[0]
+        outf = join('btrim','outfile.fastq')
+        r = self._C( p=self.truseq, t=inf, o=outf )
+        ok_( 0 != r )
+
+    def test_returns_retcode( self ):
+        from runsample import flash
+        flashout = flash( *self.fqs )
+        eq_( 0, self._C( p=self.truseq, t=flashout[0], o='somefile' ), 'Did not return return code' )
+        ok_( 0 != self._C() )
+
+class TestUnitBtrimFiles( Base ):
+    def _C( self, *args, **kwargs ):
+        from runsample import btrim_files
+        return btrim_files( *args, **kwargs )
+
+    def test_creates_outdir( self ):
+        outp = join('dir1','dir2')
+        r = self._C( self.flash(), outp, p=self.truseq )
+        ok_( exists( outp ), 'Did not create {}'.format(outp) )
+
+    def test_runs_multiple( self ):
+        outp = 'btrim'
+        inf = self.flash()
+        r = self._C( inf, outp, p=self.truseq )
+        bfiles = []
+        for f in inf:
+            ff = join( outp, basename(f).replace('.fastq','.btrim.fastq') )
+            bfiles.append(ff)
+            ok_( exists( ff ), 'Did not create {}'.format(ff) )
+        eq_( set([]), set(bfiles)-set(r) )
+
+class TestUnitRunAssembly(Base):
+    def _C( self, *args, **kwargs ):
+        from runsample import run_assembly
+        return run_assembly( *args, **kwargs )
+    
+    @attr('current')
+    def test_creates_and_runs_project( self ):
+        r = self._C( self.fqs )
+        eq_( 0, r, 'runAssembly return non-zero' )
+        projdir = glob( 'P_*' )
+        eq_( 1, len(projdir), 'Did not create a project' )
+        assxml = join(projdir[0],'assembly','454AssemblyProject.xml')
+        ok_( exists(assxml), 'Did not create project {}'.format(projdir) )
+
+    @attr('current')
+    def test_creates_named_project( self ):
+        projdir = 'projdir'
+        r = self._C( self.fqs, o=projdir )
+        eq_( 0, r, 'runAssembly return non-zero' )
+        assxml = join(projdir,'assembly','454AssemblyProject.xml')
+        ok_( exists(assxml), 'Did not create project {}'.format(projdir) )
+
+class TestUnitNewAssembly(Base):
+    def _C( self, *args, **kwargs ):
+        from runsample import new_assembly
+        return new_assembly( *args, **kwargs )
+
+    def test_projdir_set( self ):
+        projdir = 'testproject'
+        r = self._C( projdir )
+        assxml = join(projdir,'assembly','454AssemblyProject.xml')
+        ok_( exists(assxml), 'Did not create project {}'.format(projdir) )
+
+    def test_project_notset( self ):
+        r = self._C()
+        projdir = glob( 'P_*' )
+        eq_( 1, len(projdir), 'Did not create a project' )
+        eq_( projdir[0], r, 'Did not return project path correctly. {} != {}'.format(projdir,r) )
+
+class TestUnitReplaceNewblerSettings(Base):
+    def _C( self, *args, **kwargs ):
+        from runsample import replace_newbler_settings
+        return replace_newbler_settings( *args, **kwargs )
+
+    def test_returns_correct_xml( self ):
+        from runsample import new_assembly
+        # Create new assembly proj
+        projdir = new_assembly( )
+        xmlfile = join( projdir, 'assembly', '454AssemblyProject.xml' )
+        r = self._C( projdir, self.fqs )
+        xml = open(xmlfile).read()
+        print xml
+        eq_( 2, xml.count('ReadFiles'), 'Did not insert ReadFiles correctly' )
+        self.fqs_in_xml( self.fqs, xml )
+
+class TestUnitNewblerFastqReadFiles(Base):
+    def _C( self, *args, **kwargs ):
+        from runsample import newbler_fastq_read_files
+        return newbler_fastq_read_files( *args, **kwargs )
+
+    def test_correct_xml( self ):
+        r = self._C( self.fqs )
+        self.fqs_in_xml( self.fqs, r )
+
 class TestFunctional(Base):
     def _C( self, *args, **kwargs ):
         script = join( dirname(dirname(__file__)), 'runsample.py' )
@@ -76,3 +210,21 @@ class TestFunctional(Base):
         efiles = self.flash_output_files( prefix, outdir )
         for f in efiles:
             ok_( exists(f), 'Did not create flash output file {}'.format(f) )
+
+    def test_btrim_files( self ):
+        r = self._C( self.fixdir )
+        flasho = filter( lambda x: x.endswith('.fastq'), self.flash_output_files( 'out', 'flash' ) )
+        outdir = join( 'output', 'btrim' )
+        print [join(root,f) for root,dirs,files in os.walk('.') for f in files]
+        for f in flasho:
+            bfile = join(outdir,basename(f)).replace('.fastq','.btrim.fastq')
+            ok_( exists(bfile), 'Did not create btrim output file {}'.format(bfile) )
+
+    def test_newbler_files( self ):
+        r = self._C( self.fixdir, o='output' )
+        projdir = join( 'output', 'newbler_assembly' )
+        xmlp = join( projdir, 'assembly', '454AssemblyProject.xml' )
+        xml = open(xmlp).read()
+        btrimo = join( 'output', 'btrim' )
+        btrimfqs = glob( join(btrimo,'*.fastq') )
+        self.fqs_in_xml( btrimfqs, xml )
