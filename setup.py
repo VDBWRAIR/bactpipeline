4855cc2a3e2495397abce09546b3fc075e42d8ea
Tyghe Vallard <vallardt@gmail.com>
2015-12-15 15:24:46 -0500

    Removing unwanted old symlinks. Adding travis/tox. Adding setup.py with btrim and flash install(builds flash). Fixes for python2.6 support

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
 from setuptools import setup, find_packages
 
 import bactpipeline
+import subprocess
+import sys
+from os.path import join, exists
+
+flash_path = 'dependencies/FLASH-1.2.10'
+flash_bin = join(flash_path, 'flash')
+if not exists(flash_bin):
+    try:
+        subprocess.check_call('make', cwd=flash_path)
+    except subprocess.CalledProcessError as e:
+        print "Failed to build flash prior to install."
+        print "Likely you require some system packages in order to compile it"
+        sys.exit(1)
 
 setup(
     name = bactpipeline.__projectname__,
@@ -11,9 +24,14 @@ setup(
     description = bactpipeline.__description__,
     license = "GPLv2",
     keywords = bactpipeline.__keywords__,
+    scripts = [
+        'dependencies/FLASH-1.2.10/flash',
+        'dependencies/btrim/btrim64-static',
+    ],
     entry_points = {
         'console_scripts': [
-            'runsample = bactpipeline.runsample:main'
+            'runsample = bactpipeline.runsample:main',
+            'fix_fastq = bactpipeline.fix_fastq:main',
         ],
     },
 )
